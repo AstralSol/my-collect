779b7e3f456bf399feeb4427cd6ef39e005b9b35
AstralSol <porphyrios33@gmail.com>
2020-10-08 23:32:12 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array)
+    i = 0
+        new_array = []
+    while i < array.length
+        new_array << yield(array[i])
+        i += 1
+    end
+    new_array
+end
